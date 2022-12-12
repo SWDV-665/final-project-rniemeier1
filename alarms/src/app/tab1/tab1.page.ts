b945b968d5fc9261ff6d6b69b43302e28bcb5b26
R. Niemeier <101990166+rniemeier1@users.noreply.github.com>
2022-12-11 19:10:07 -0600

    Adding/removing alarms

--- alarms/src/app/tab1/tab1.page.ts
+++ alarms/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,8 +9,9 @@ import { Component } from '@angular/core';
 export class Tab1Page {
   title = "Alarms";
 
+  tempDateTime = new Date();
 
-  constructor() {}
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {}
   alarms = [
     {
       name: "alarm1",
@@ -32,4 +34,116 @@ export class Tab1Page {
       enabled: false
     },
   ];
+
+  addAlarm(){
+    const prompt = this.alertCtrl.create({
+      header: "Add Alarm",
+      message: "Set the time for your new alarm. ",
+      inputs:[
+        {
+          name: "Alarm Name",
+          placeholder: "Alarm " + this.alarms.length + 1,
+          value: null
+        },
+        {
+          name: "Alarm Time",
+          placeholder: this.tempDateTime.getHours() + ":" + (this.tempDateTime.getMinutes() < 10 ? "0" + this.tempDateTime.getMinutes() : this.tempDateTime.getMinutes())
+          
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel Clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: async item => {
+          /*  if(index !== undefined){
+              //edit item
+              this.DataService.editItem(alarm, index);
+              const toast = this.toastCtrl.create({
+                message: 'Updated ' + alarm.name,
+                duration: 3000
+              });
+              (await (toast)).present();
+            }
+            else{
+              //add item
+              this.DataService.addItem(alarm);
+              const toast = this.toastCtrl.create({
+                message: 'Added ' + alarm.name,
+                duration: 3000
+              });
+              (await (toast)).present();
+            } */
+          }
+        }
+      ]
+    })
+  }
+
+  async deleteAlarm(alarm: { name: string; time: Date, enabled: boolean }, index: number){
+    const toast = this.toastCtrl.create({
+      message: "removing alarm" + alarm.name,
+      duration: 3000
+    });
+    (await (toast)).present();
+  }
+
+  //input service
+  /*
+  async showPrompt(item?, index?){
+    const prompt = this.alertCtrl.create({
+      header: item ? "Edit Item" : "Add Item",
+      message: item ? "Please edit the item and quantity as needed." : "Please enter the item name and quantity.",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+          value: item ? item.name : null
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity',
+          value: item ? item.quantity : null
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel Clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: async item => {
+            if(index !== undefined){
+              //edit item
+              this.DataService.editItem(item, index);
+              const toast = this.toastCtrl.create({
+                message: 'Updated ' + item.name,
+                duration: 3000
+              });
+              (await (toast)).present();
+            }
+            else{
+              //add item
+              this.DataService.addItem(item);
+            const toast = this.toastCtrl.create({
+              message: 'Added ' + item.name,
+              duration: 3000
+            });
+            (await (toast)).present();
+            }
+          }
+        }
+      ]
+     });
+    (await prompt).present();
+  }*/
+
 }
