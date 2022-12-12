6b2aa1fe33197ca32f175696282259ecc2e9e9a8
R. Niemeier <101990166+rniemeier1@users.noreply.github.com>
2022-12-11 18:16:38 -0600

    Adding alarms and settings

--- alarms/src/app/tab1/tab1.page.ts
+++ alarms/src/app/tab1/tab1.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  title = "Alarms";
+
 
   constructor() {}
-
+  alarms = [
+    {
+      name: "alarm1",
+      time: new Date(),
+      enabled: true
+    },
+    {
+      name: "alarm2",
+      time: new Date(),
+      enabled: true
+    },
+    {
+      name: "alarm3",
+      time: new Date(),
+      enabled: true
+    },
+    {
+      name: "alarm4",
+      time: new Date(),
+      enabled: false
+    },
+  ];
 }
